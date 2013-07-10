62d83b6368e3ec6f0e86af0ad40c39a1d2224827
jcdenton <ivan.bruzgin@gmail.com>
2013-07-10 08:22:07 +0300

    Updated admin imports

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,7 +1,4 @@
 # -*- encoding: utf-8 -*-
 from django.contrib import admin
 
-from admins import player
-from admins import referee
-from admins import round
-from admins import tournament
+import admins
--- tournament/admins/__init__.py
+++ tournament/admins/__init__.py
@@ -1,1 +1,7 @@
 # -*- encoding: utf-8 -*-
+
+import game
+import player
+import referee
+import round
+import tournament
