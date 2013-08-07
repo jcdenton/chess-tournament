492279b3e49707134770d6b1cf4a781bb9bba809
jcdenton <ivan.bruzgin@gmail.com>
2013-08-08 01:57:40 +0300

    Fixed url config

--- chess/urls.py
+++ chess/urls.py
@@ -1,10 +1,12 @@
 # -*- encoding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    url(r'^tournament/', include('tournament.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',
+                       url(r'^$', RedirectView.as_view(url='tournament/')),
+                       url(r'^tournament/', include('tournament.urls', namespace='tournament')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
