9f4f15e8281a7a57acbc714c51a9c66f090e5dc0
jcdenton <ivan.bruzgin@gmail.com>
2013-08-08 10:17:29 +0300

    Added guest views

--- tournament/urls.py
+++ tournament/urls.py
@@ -1,4 +1,10 @@
 # -*- encoding: utf-8 -*-
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
+from django.views import generic
 
-urlpatterns = patterns('')
+from . import views
+
+urlpatterns = patterns('',
+                       url(r'^$', generic.RedirectView.as_view(url='tournaments/'), name='index'),
+                       url(r'^tournaments/', views.TournamentsView.as_view(), name='tournaments'),
+                       )
--- tournament/views.py
+++ tournament/views.py
@@ -1,1 +1,11 @@
 # -*- encoding: utf-8 -*-
+from django.views import generic
+
+from .models import Tournament
+
+
+class TournamentsView(generic.ListView):
+    context_object_name = 'tournaments'
+
+    def get_queryset(self):
+        return Tournament.objects.order_by('-start_date')
