b1aca29fbd386c22f666047b4245ebb1d492ca6a
jcdenton <ivan.bruzgin@gmail.com>
2013-08-14 10:34:11 +0300

    Added Tournaments detail view

--- tournament/urls.py
+++ tournament/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = patterns('',
                        url(r'^$', generic.RedirectView.as_view(url='tournaments/'), name='index'),
                        url(r'^tournaments/$', views.TournamentListView.as_view(), name='tournament_list'),
+                       url(r'^tournaments/(?P<pk>\d+)/$', views.TournamentDetailView.as_view(), name='tournament_detail'),
                        )
--- tournament/views.py
+++ tournament/views.py
@@ -9,3 +9,8 @@ class TournamentListView(generic.ListView):
 
     def get_queryset(self):
         return Tournament.objects.order_by('-start_date')
+
+
+class TournamentDetailView(generic.DetailView):
+    model = Tournament
+    context_object_name = 'tournament'
