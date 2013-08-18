6c96a5c7a4fdf59d554836c727334e16fdff5c3b
jcdenton <ivan.bruzgin@gmail.com>
2013-08-18 22:42:09 +0300

    Fixed django_countries flags images issue

--- chess/settings.py
+++ chess/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     # 'django.contrib.admindocs',
+    'django_countries',
     'djangojs',
     'tournament',
 )
