06b834760eaa69e8c304a5abef8e3780aef263c8
jcdenton <ivan.bruzgin@gmail.com>
2013-08-20 11:32:25 +0300

    Improved Game model admin

--- tournament/admins/game.py
+++ tournament/admins/game.py
@@ -2,18 +2,20 @@
 from django.contrib import admin
 
 from ..models import Game, Score
-from .utils import ForbidAddMixin, ForbidDeleteMixin, get_fk_field_link, CustomStackedInline
+from .utils import ForbidAddMixin, get_fk_field_link, CustomStackedInline
 
 
-class ScoreInline(ForbidAddMixin, ForbidDeleteMixin, CustomStackedInline):
+class ScoreInline(ForbidAddMixin, CustomStackedInline):
     model = Score
     extra = 0
 
 
 class GameAdmin(ForbidAddMixin, admin.ModelAdmin):
+    list_display = ('__unicode__', 'white', 'black', 'winner', 'round_link', 'start_date', 'end_date')
     exclude = ('round',)
     readonly_fields = ('round_link',)
     inlines = (ScoreInline,)
+    search_fields = ('tournament',)
 
     round_link = get_fk_field_link('round', 'Round', 'round')
 
